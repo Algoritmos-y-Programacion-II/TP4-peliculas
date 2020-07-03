7c93c24eb0cdeaa50fd4f7b0b14a42b4e0d254fa
root-MKevin <kmeaca@fi.uba.ar>
2020-07-03 02:22:48 -0300

    Se hicieron todas las modificaciones necesarias y ademas se agregaron atributos y metodos para que cumpla con la nueva funcionalidad. Faltan PRE y POST de los metodos nuevos.

--- PeliculasRecomendadas.cpp
+++ PeliculasRecomendadas.cpp
@@ -1,11 +1,11 @@
 //
-// Created by daseos on 28/6/20.
-//
+// Created by root-MKevin on 28/6/20.
+// Los metodos se encuentran ordenados alfabeticamente sin distincion del tipo que sea.
 
 #include "PeliculasRecomendadas.h"
 
-bool PeliculasRecomendadas::actorEncontrado(Pelicula *peliculaNoVista, Lista<string> *actoresVistos){
-    Lista<string> *listaAuxiliar = peliculaNoVista->obtenerActores();
+bool PeliculasRecomendadas::actorEncontrado(Pelicula *peliculaLeida, Lista<string> *actoresVistos){
+    Lista<string> *listaAuxiliar = peliculaLeida->obtenerActores();
     bool actorEnLista = false;
     int i = 0;
     while( (i < listaAuxiliar->obtenerCantidadElementos()) && !actorEnLista ){
@@ -18,17 +18,16 @@ bool PeliculasRecomendadas::actorEncontrado(Pelicula *peliculaNoVista, Lista<str
     return actorEnLista;
 }
 
-void PeliculasRecomendadas::cargarActores(Pelicula *peliculaVista, Lista <string> *&actoresVistos){
-    Lista <string> *listaAuxiliar;
+void PeliculasRecomendadas::cargarActoresLista(Pelicula *peliculaVista, Lista <string> *actoresVistos){
+
     if(actoresVistos->obtenerCantidadElementos() == 0){
-        listaAuxiliar = peliculaVista->obtenerActores();
-        for(int i=0; listaAuxiliar->obtenerCantidadElementos(); i++){
-            actoresVistos->agregarAlFinal(listaAuxiliar->obtenerDato(i));
+        for(int i=0; i < peliculaVista->obtenerActores()->obtenerCantidadElementos(); i++){
+            actoresVistos->agregarAlFinal(peliculaVista->obtenerActores()->obtenerDato(i));
         }
     }
     else{
-        for(int i=0; listaAuxiliar->obtenerCantidadElementos(); i++){
-            string actorLeido = listaAuxiliar->obtenerDato(i);
+        for(int i=0; i < peliculaVista->obtenerActores()->obtenerCantidadElementos(); i++){
+            string actorLeido = peliculaVista->obtenerActores()->obtenerDato(i);
             if( !( actoresVistos->datoEnLista(actorLeido) ) ){
                 actoresVistos->agregarAlFinal(actorLeido);
             }
@@ -36,7 +35,26 @@ void PeliculasRecomendadas::cargarActores(Pelicula *peliculaVista, Lista <string
     }
 }
 
-void PeliculasRecomendadas::cargarDirector(Pelicula *peliculaVista, Lista<string> *&directoresVistos){
+void PeliculasRecomendadas::cargarActoresPelicula(Pelicula *&peliculaLeida, Pelicula *&peliculaRecomendada){
+    Lista<string> *listaAux = peliculaLeida->obtenerActores();
+    for(int i =0; i< listaAux->obtenerCantidadElementos(); i++){
+        peliculaRecomendada->cargarActor(listaAux->obtenerDato(i));
+    }
+}
+
+
+void PeliculasRecomendadas::cargarDatosPelicula(Pelicula *&peliculaLeida, Pelicula *&peliculaRecomendada){
+
+    peliculaRecomendada->cargarTitulo(peliculaLeida->obtenerTitulo());
+    peliculaRecomendada->cargarGenero(peliculaLeida->obtenerGenero());
+    peliculaRecomendada->cargarDirector(peliculaLeida->obtenerDirector());
+    peliculaRecomendada->cargarPuntaje(peliculaLeida->obtenerPuntaje());
+    cargarActoresPelicula(peliculaLeida, peliculaRecomendada);
+
+}
+
+
+void PeliculasRecomendadas::cargarDirector(Pelicula *peliculaVista, Lista<string> *directoresVistos){
 
     string directorAuxiliar = peliculaVista->obtenerDirector();
     if( !( directoresVistos->datoEnLista(directorAuxiliar) ) ){
@@ -44,65 +62,139 @@ void PeliculasRecomendadas::cargarDirector(Pelicula *peliculaVista, Lista<string
     }
 }
 
-void PeliculasRecomendadas::cargarGenero(Pelicula *peliculaVista, Lista<string> *&generosVistos){
+void PeliculasRecomendadas::cargarGenero(Pelicula *peliculaVista, Lista<string> *generosVistos){
 
     string generoAuxiliar = peliculaVista->obtenerGenero();
-    if( !( generosVistos->datoEnLinea(generoAuxiliar) ) ){
+    if( !( generosVistos->datoEnLista(generoAuxiliar) ) ){
         generosVistos->agregarAlFinal(generoAuxiliar);
     }
 }
 
-void PeliculasRecomendadas::cargarListasAuxiliares(Lista<Pelicula> *peliculasVistas, Lista<string> *&generosVistos,
-                                                   Lista<string> *&directoresVistos, Lista<string> *&actoresVistos){
-    Pelicula peliculaAuxiliar;
-    for(int i =0; peliculasVistas->obtenerCantidadElementos(); i++){
-        peliculaAuxiliar = peliculasVistas->obtenerDato(i);
+void PeliculasRecomendadas::cargarListasAuxiliares(Lista<string> *generosVistos, Lista<string> *directoresVistos,
+        Lista<string> *actoresVistos){
+
+    for(int i =0; i < peliculasVistas.obtenerCantidadElementos(); i++){
+        Pelicula *peliculaAuxiliar = peliculasVistas.obtenerDato(i);
         if(i==0){
-            generosVistos->agregarAlFinal(peliculaAuxiliar.obtenerGenero());
-            directoresVistos->agregarAlFinal(peliculaAuxiliar.obtenerDirector());
-            cargarActores(&peliculaAuxiliar, actoresVistos);
+            generosVistos->agregarAlFinal(peliculaAuxiliar->obtenerGenero());
+            directoresVistos->agregarAlFinal(peliculaAuxiliar->obtenerDirector());
+            cargarActoresLista(peliculaAuxiliar, actoresVistos);
         }
         else{
-            cargarGenero(&peliculaAuxiliar, generosVistos);
-            cargarDirector(&peliculaAuxiliar, directoresVistos);
-            cargarActores(&peliculaAuxiliar, actoresVistos);
+            cargarGenero(peliculaAuxiliar, generosVistos);
+            cargarDirector(peliculaAuxiliar, directoresVistos);
+            cargarActoresLista(peliculaAuxiliar, actoresVistos);
         }
     }
 }
 
-void PeliculasRecomendadas::recomendarPeliculas(Lista<Pelicula> *peliculasNoVistas){
-    int puntajePeliculaLeida;
-    for(int i=0; peliculasNoVistas->obtenerCantidadElementos(); i++){
-        puntajePeliculaLeida = (peliculasNoVistas->obtenerDato(i)).obtenerPuntaje();
-        if(puntajePeliculaLeida >= PUNTAJE_VALIDO){
-            listaPeliculasRecomendadas.agregarAlFinal(peliculasNoVistas->obtenerDato(i));
+void PeliculasRecomendadas::cargarListasDePeliculas(){
+    cargador.cargar(ARCHIVO_VISTAS, peliculasVistas);
+    cargador.cargar(ARCHIVO_NO_VISTAS, peliculasNoVistas);
+}
+
+void PeliculasRecomendadas::cargarPelicula(Pelicula *&peliculaLeida){
+    Pelicula *peliculaRecomendada = new Pelicula;
+    cargarDatosPelicula(peliculaLeida,peliculaRecomendada);
+    peliculasRecomendadas.agregarAlFinal(peliculaRecomendada);
+}
+
+void PeliculasRecomendadas::imprimirActores(Lista<string> *actores){
+    std::cout << "REPARTO: ";
+    for(int i=0; i < actores->obtenerCantidadElementos(); i++){
+        std::cout << actores->obtenerDato(i) << " ";
+    }
+}
+
+void PeliculasRecomendadas::imprimirDatosPelicula(Pelicula *peliculaLeida){
+    std::cout << "\nTITULO: " << peliculaLeida->obtenerTitulo() << std::endl;
+    std::cout << "GENERO: " << peliculaLeida->obtenerGenero() << std::endl;
+    std::cout << "PUNTAJE: " << peliculaLeida->obtenerPuntaje() << std::endl;
+    std::cout << "DIRECTOR: " << peliculaLeida->obtenerDirector() << std::endl;
+    imprimirActores(peliculaLeida->obtenerActores());
+}
+
+void PeliculasRecomendadas::liberarMemoria(){
+    if(peliculasVistas.obtenerCantidadElementos() != 0) {
+        for (int i = 0; i < peliculasVistas.obtenerCantidadElementos(); i++) {
+            delete peliculasVistas.obtenerDato(i);
+        }
+    }
+    if(peliculasNoVistas.obtenerCantidadElementos() != 0) {
+        for (int j = 0; j < peliculasNoVistas.obtenerCantidadElementos(); j++) {
+            delete peliculasNoVistas.obtenerDato(j);
+        }
+    }
+    if(peliculasRecomendadas.obtenerCantidadElementos() !=0 ){
+        for (int k = 0; k < peliculasRecomendadas.obtenerCantidadElementos(); k++) {
+            delete peliculasRecomendadas.obtenerDato(k);
         }
     }
 }
 
-void PeliculasRecomendadas::recomendarPeliculas(Lista<Pelicula> *peliculasVistas, Lista<Pelicula> *peliculasNoVistas){
-    Lista<string> *generosVistos;
-    Lista<string> *directoresVistos;
-    Lista<string> *actoresVistos;
 
-    cargarListasAuxiliares(peliculasVistas, generosVistos, directoresVistos, actoresVistos);
+void PeliculasRecomendadas::mostrarPeliculasVistas(){
+    for(int i=0; i < peliculasVistas.obtenerCantidadElementos(); i++){
+        imprimirDatosPelicula(peliculasVistas.obtenerDato(i));
+    }
+}
 
-    Pelicula peliculaLeida;
-    for(int i=0; peliculasNoVistas->obtenerCantidadElementos(); i++){
-        peliculaLeida = peliculasNoVistas->obtenerDato(i);
-        string generoLeido = peliculaLeida.obtenerGenero();
-        if( generosVistos->datoEnLista(generoLeido) ){
-            string directorLeido = peliculaLeida.obtenerDirector();
-            if( directoresVistos->datoEnLista(directorLeido) || actorEncontrado(&peliculaLeida, actoresVistos) ){
-                listaPeliculasRecomendadas.agregarAlFinal(peliculaLeida);
+void PeliculasRecomendadas::mostrarPeliculasNoVistas(){
+    for(int i = 0; i < peliculasNoVistas.obtenerCantidadElementos(); i++){
+        imprimirDatosPelicula(peliculasNoVistas.obtenerDato(i));
+    }
+}
+
+
+void PeliculasRecomendadas::mostrarPeliculasRecomendadas(){
+    for(int i=0; i< peliculasRecomendadas.obtenerCantidadElementos(); i++){
+        imprimirDatosPelicula(peliculasRecomendadas.obtenerDato(i));
+    }
+}
+
+void PeliculasRecomendadas::recomendarPeliculas(){
+    Lista<string> generosVistos;
+    Lista<string> directoresVistos;
+    Lista<string> actoresVistos;
+
+    cargarListasAuxiliares(&generosVistos, &directoresVistos, &actoresVistos);
+
+    for(int i=0; i < peliculasNoVistas.obtenerCantidadElementos(); i++){
+        Pelicula *peliculaLeida = peliculasNoVistas.obtenerDato(i);
+        string generoLeido = peliculaLeida->obtenerGenero();
+        if( generosVistos.datoEnLista(generoLeido) ){
+            string directorLeido = peliculaLeida->obtenerDirector();
+            if( directoresVistos.datoEnLista(directorLeido) || actorEncontrado(peliculaLeida, &actoresVistos) ){
+                cargarPelicula(peliculaLeida);
             }
         }
-        else if(peliculaLeida.obtenerPuntaje() >= PUNTAJE_VALIDO){
-            listaPeliculasRecomendadas.agregarAlFinal(peliculaLeida);
+        else if(peliculaLeida->obtenerPuntaje() >= PUNTAJE_VALIDO){
+            cargarPelicula(peliculaLeida);
         }
     }
 }
 
-Lista<Pelicula> *PeliculasRecomendadas::obtenerPeliculasRecomendadas(){
-    return &listaPeliculasRecomendadas;
+void PeliculasRecomendadas::recomendarPorPuntaje(){
+    for(int i=0; i < peliculasNoVistas.obtenerCantidadElementos(); i++){
+        Pelicula *peliculaLeida = peliculasNoVistas.obtenerDato(i);
+        if(peliculaLeida->obtenerPuntaje() >= PUNTAJE_VALIDO){
+            cargarPelicula(peliculaLeida);
+        }
+    }
+}
+
+void PeliculasRecomendadas::verPeliculasRecomendadas(){
+    if(peliculasVistas.obtenerCantidadElementos() != 0){
+        recomendarPeliculas();
+    }
+    else{
+        if(peliculasNoVistas.obtenerCantidadElementos() != 0) {
+            recomendarPorPuntaje();
+        }
+    }
+    mostrarPeliculasRecomendadas();
+}
+
+PeliculasRecomendadas::~PeliculasRecomendadas(){
+    liberarMemoria();
 }
--- PeliculasRecomendadas.h
+++ PeliculasRecomendadas.h
@@ -1,12 +1,17 @@
 //
 // Created by root-MKevin on 28/6/20.
-//
+// Los metodos se encuentran ordenados por el tipo (Modificador, Consultor, etc.)
+// y ademas, dentro de cada tipo, ordenados alfabeticamente.
 
 #ifndef PELICULASRECOMENDADAS_H
 #define PELICULASRECOMENDADAS_H
 
 #include "Lista.h"
 #include "Pelicula.h"
+#include "CargadorDePeliculas.h"
+
+const string ARCHIVO_VISTAS = "peliculasVistas.txt";
+const string ARCHIVO_NO_VISTAS = "peliculasNoVistas.txt";
 
 const int PUNTAJE_VALIDO = 7;
 
@@ -14,7 +19,10 @@ class PeliculasRecomendadas {
 
     private:
         // ATRIBUTOS
-        Lista<Pelicula> listaPeliculasRecomendadas;
+        Lista<Pelicula*> peliculasVistas;
+        Lista<Pelicula*> peliculasNoVistas;
+        Lista<Pelicula*> peliculasRecomendadas;
+        CargadorDePeliculas cargador;
 
         // METODOS
 
@@ -23,58 +31,94 @@ class PeliculasRecomendadas {
         //PRE: RECIBE UN PUNTERO DE TIPO PELICULA Y UNA LISTA DE STRINGS (PUEDE TENER O NO TENER DATOS).
         //POST: NO DEVUELVE NADA. SI LA LISTA ESTA VACIA, CARGA TODOS LOS ACTORES. SI YA TIENE DATOS CARGADOS
         //      COMPARA LOS DATOS PARA NO CARGAR UN DATO EXISTENTE.
-        void cargarActores(Pelicula *peliculaVista, Lista<string> *&actoresVistos);
+        void cargarActoresLista(Pelicula *peliculaVista, Lista<string> *actoresVistos);
 
-        //PRE: RECIBE UN PUNTERO DE TIPO PELICULA Y UNA LISTA DE STRINGS PREVIAMENTE CARGADA.
-        //POST: NO DEVUELVE NADA. SI EL GENERO DE LA PELICULA ESTA EN LA LISTA NO HACE NADA,
-        //      DE LO CONTRARIO, LO CARGA.
-        void cargarGenero(Pelicula *peliculaVista, Lista<string> *&generosVistos);
+        //PRE:
+        //POST:
+        void cargarActoresPelicula(Pelicula *&peliculaLeida, Pelicula *&peliculaRecomendada);
+
+        //PRE:
+        //POST:
+        void cargarDatosPelicula(Pelicula *&peliculaLeida, Pelicula *&peliculaRecomendada);
+
 
         //PRE: RECIBE UN PUNTERO DE TIPO PELICULA Y UNA LISTA DE STRINGS PREVIAMENTE CARGADA.
         //POST: NO DEVUELVE NADA. SI EL DIRECTOR DE LA PELICULA ESTA EN LA LISTA NO HACE NADA,
         //      DE LO CONTRARIO, LO CARGA.
-        void cargarDirector(Pelicula *peliculaVista, Lista<string> *&directoresVistos);
+        void cargarDirector(Pelicula *peliculaVista, Lista<string> *directoresVistos);
+
+        //PRE: RECIBE UN PUNTERO DE TIPO PELICULA Y UNA LISTA DE STRINGS PREVIAMENTE CARGADA.
+        //POST: NO DEVUELVE NADA. SI EL GENERO DE LA PELICULA ESTA EN LA LISTA NO HACE NADA,
+        //      DE LO CONTRARIO, LO CARGA.
+        void cargarGenero(Pelicula *peliculaVista, Lista<string> *generosVistos);
 
         //PRE: RECIBE UN PUNTERO DE TIPO LISTA, DE TIPO PELICULA, Y TRES PUNTEROS DE TIPO LISTA, DE TIPO STRING.
         //     LA LISTA DE TIPO PELICULAS DEBE ESTAR PREVIAMENTE CARGADA. LAS OTRAS DEBEN ESTAR VACIAS.
         //POST: NO DEVUELVE NADA. CARGA LOS DATOS CORRESPONDIENTES EN CADA UNA DE LAS LISTAS.
-        void cargarListasAuxiliares(Lista<Pelicula> *peliculasVistas, Lista<string> *&generosVistos,
-                                Lista<string> *&directoresVistos, Lista<string> *&actoresVistos);
+        void cargarListasAuxiliares(Lista<string> *generosVistos, Lista<string> *directoresVistos,
+                Lista<string> *actoresVistos);
+
+        //PRE:
+        //POST:
+        void cargarListasDePeliculas();
+
+        //PRE:
+        //POST:
+        void cargarPelicula(Pelicula *&peliculaLeida);
+
+        //PRE:
+        //POST:
+        void liberarMemoria();
+
+        //PRE: RECIBE UN PUNTERO DE TIPO LISTA DE PELICULA, EN PARTICULAR LAS NO VISTAS, PREVIAMENTE CARGADA.
+        //POST: GENERA UNA NUEVA LISTA DE PELICULAS RECOMENDADAS A PARTIR DE PELICULAS CON PUNTAJE MAYOR O IGUAL A 7.
+        void recomendarPorPuntaje();
+
+        //PRE: RECIBE DOS PUNTERO DE TIPO LISTA DE PELICULA, PREVIAMENTE CARGADAS.
+        //POST: GENERA UNA NUEVA LISTA DE PELICULAS RECOMENDADAS, EN FUNCION DEL GENERO Y DIRECTOR/ACTORES QUE
+        //      PARTICIPAN O DE UN PUNTAJE MAYOR O IGUAL A 7.
+        void recomendarPeliculas();
 
         // CONSULTORES
 
         //PRE: RECIBE UNA PUNTERO DE TIPO PELICULA Y UNA LISTA DE STRING PREVIAMENTE CARGADA.
         //POST: DEVUELVE TRUE SI EL ACTOR ESTA EN LA LISTA, FALSE DE LO CONTRARIO.
-        bool actorEncontrado(Pelicula *peliculaNoVista, Lista<string> *actoresVistos);
+        bool actorEncontrado(Pelicula *peliculaLeida, Lista<string> *actoresVistos);
 
+        //PRE:
+        //POST:
+        void imprimirActores(Lista<string> *actores);
+
+        //PRE:
+        //POST:
+        void imprimirDatosPelicula(Pelicula* peliculaLeida);
+
+        //PRE:
+        //POST:
+        void mostrarPeliculasRecomendadas();
     public:
 
         // METODOS
 
-        // CONSTRUCTOR
-
-        //PRE: NO RECIBE NADA.
-        //POST: CREA UN OBJETO DE TIPO PeliculasRecomendadas.
-
-        PeliculasRecomendadas();
-
-        // MODIFICADOR
-
-        //PRE: RECIBE UN PUNTERO DE TIPO LISTA DE PELICULA, EN PARTICULAR LAS NO VISTAS, PREVIAMENTE CARGADA.
-        //POST: GENERA UNA NUEVA LISTA DE PELICULAS RECOMENDADAS A PARTIR DE PELICULAS CON PUNTAJE MAYOR O IGUAL A 7.
-        void recomendarPeliculas(Lista<Pelicula> *peliculasNoVistas);
-
-        //PRE: RECIBE DOS PUNTERO DE TIPO LISTA DE PELICULA, PREVIAMENTE CARGADAS.
-        //POST: GENERA UNA NUEVA LISTA DE PELICULAS RECOMENDADAS, EN FUNCION DEL GENERO Y DIRECTOR/ACTORES QUE
-        //      PARTICIPAN O DE UN PUNTAJE MAYOR O IGUAL A 7.
-        void recomendarPeliculas(Lista<Pelicula> *peliculasVistas, Lista<Pelicula> *peliculasNoVistas);
-
         // CONSULTORES
 
-        //PRE: NO RECIBE NADA.
-        //POST: DEVUELVE UN PUNTERO DE TIPO LISTA DE PELICULA, DIRECCIONADO AL ATRIBUTO listaPeliculasRecomendadas.
-        Lista<Pelicula> *obtenerPeliculasRecomendadas();
+        //PRE:
+        //POST:
+        void mostrarPeliculasVistas();
 
+        //PRE:
+        //POST:
+        void mostrarPeliculasNoVistas();
+
+        //PRE:
+        //POST:
+        void verPeliculasRecomendadas();
+
+        // DESTRUCTOR
+
+        //PRE:
+        //POST:
+        ~PeliculasRecomendadas();
 };
 
 
