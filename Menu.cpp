dfd0ae717e0d78176f2d9695be7ec0a5489eba55
Valentina Varela Rodriguez <vavarela.rodriguez@gmail.com>
2020-07-03 22:39:57 -0300

    Fix

--- Menu.cpp
+++ Menu.cpp
@@ -1,83 +1,60 @@
-#include <iostream>
 #include "Menu.h"
-
-Menu::Menu(Lista<Pelicula> peliculasVistas, Lista<Pelicula> peliculasNoVistas, Lista<Pelicula> peliculasRecomendadas) {
-
-    this->peliculasVistas = peliculasVistas;
-    this->peliculasNoVistas = peliculasNoVistas;
-    this->peliculasRecomendadas = peliculasRecomendadas;
-    
-}
+#include "Lista.h"
+#include <iostream>
 
 void Menu::mostrarMenu(){
 
-    int opcion;
-
-    cout << "1.Mostrar peliculas vistas." <<endl;
-
-    cout << "2.Mostrar peliculas no vistas." <<endl;
-
-    cout << "3.Mostrar peliculas recomendadas." <<endl;
-
-    cout << "4.Salir." <<endl;
-
-    cout << "Ingrese el numero de la opcion que desea seleccionar: " <<endl;
-
+    cout << "\t1. Mostrar peliculas vistas\n";
+    cout << "\t2. Mostrar peliculas no vistas\n";
+    cout << "\t3. Mostrar peliculas recomendadas\n";
+    cout << "\t4. Salir\n";
+    cout << "\tIngrese el numero de la opcion que desea seleccionar: ";
     cin >> opcion;
 
-    this->opcion = opcion;
-
 }
 
-bool Menu::SelectorDeOpciones() {
-    
+bool Menu::selectorDeOpciones() {
+
+    bool estado;
+    Lista<Pelicula*>* lista;
+
     switch (this->opcion) {
-        case 1:{
 
-            iterarListaPeliculasVistas();
-            return true;
+        case 1: {
+            cout << "\n\t---------------------- PELICULAS VISTAS ----------------------\n";
+            peliculasRecomendadas.mostrarPeliculasVistas();
+            cout << "\n\t--------------------------------------------------------------\n";
+
+            estado = true;
             break;
         }
 
-        case 2:{
-
-            iterarListaPeliculasNoVistas();
-            return true;
-            break;
-
-        }
-
-        case 3:{
-
-            iterarListaPeliculasRecomendadas();
-            return true;
+        case 2: {
+            cout << "\t--------------------- PELICULAS NO VISTAS --------------------\n";
+            peliculasRecomendadas.mostrarPeliculasNoVistas();
+            cout << "\n\t--------------------------------------------------------------\n";
+            estado = true;
             break;
         }
 
-        case 4:{
-
-            return false;
+        case 3: {
+            cout << "\t-------------------- PELICULAS RECOMENDADAS ------------------\n";
+            peliculasRecomendadas.mostrarPeliculasRecomendadas();
+            cout << "\n\t--------------------------------------------------------------\n";
+            estado = true;
             break;
         }
 
-        default:{
-            cout << "Opcion no valida. Ingrese alguna de las opciones en el menu" << endl;
-            return true;
+        case 4: {
+            estado = false;
             break;
-
         }
 
+        default: {
+            cout << "\tOpcion no valida. Ingrese alguna de las opciones en el menu" << endl;
+            estado = true;
+            break;
+        }
     }
-}
-
-void Menu::iterarListaPeliculasVistas(){
-    cout << "Placeholder"<< endl;
-}
-
-void Menu::iterarListaPeliculasNoVistas(){
-    cout << "Placeholder"<< endl;
-}
-
-void Menu::iterarListaPeliculasRecomendadas(){
-    cout << "Placeholder"<< endl;
+    return estado;
 }
--- Menu.h
+++ Menu.h
@@ -1,49 +1,26 @@
 #ifndef PELICULAS_MENU_H
 #define PELICULAS_MENU_H
 
-#include "Lista.h"
-#include <fstream>
-#include <sstream>
+#include "PeliculasRecomendadas.h"
 
 using namespace std;
 
 class Menu{
 
 private:
-
-Lista<Pelicula> peliculasVistas;
-
-Lista<Pelicula> peliculasNoVistas;
-
-Lista<Pelicula> peliculasRecomendadas;
-
-int opcion;
-
-void iterarListaPeliculasVistas();
-
-void iterarListaPeliculasNoVistas();
-
-void iterarListaPeliculasRecomendadas();
+    PeliculasRecomendadas peliculasRecomendadas;
+    int opcion;
 
 public:
-
-    //Constructor con parametro
-    //Pre-Condicion: Las listas deben existir
-    //Post-Condicion: Se crea un objeto del tipo Menu
-
-    Menu(Lista<Pelicula> peliculasVistas,Lista<Pelicula> peliculasNoVistas,Lista<Pelicula> peliculasRecomendadas);
 
     //Mostrar las opciones que el usuario puede elegir
     //Pre-Condicion: Debe instanciarse un menu y pasar las listas por parametro
     //Post-Condicion: Imprime por pantalla las decisiones que tiene el usuario
-
     void mostrarMenu();
 
     //Procesa la opcion del usuario
     //Pre-Condicion: Debe existir un objeto instanciado Menu y la opcion debe estar entre el rango 1 y el 3
     //Post-Condicion: procesa la opcion invocando las funciones correspondientes
-
-    bool SelectorDeOpciones();
-
+    bool selectorDeOpciones();
 };
 #endif //PELICULAS_MENU_H
