5bd72fce3dea18a2bb801968bca9f67ce98cc6aa
Valentina Varela Rodriguez <vavarela.rodriguez@gmail.com>
2020-07-06 11:04:38 -0300

    Added main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
-#include <iostream>
+/*
+  75.41/95.15: Algoritmos y Programacion II - Catedra Juarez - 1°C 2020
+  Trabajo Practico Grupal N4: Recomendacion de Peliculas
+  Hecho por
+            Morgan Fossati
+            Patricio Rodriguez
+            Kevin Meaca
+            Valentina Varela Rodriguez
+*/
+
+#include "Menu.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    Menu menu;
+    menu.mostrarMenu();
+    while (menu.selectorDeOpciones()) {
+        menu.mostrarMenu();
+    }
+
     return 0;
 }
